7b796447ef9c7230bb42581341bf2427ab666666
SujayKumarMondal <sujay@surelia.info>
2025-03-10 06:26:34 +0530

    fixed minor changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo " BUILD START"
 python 3.12 -m pip install requirements.txt
-python 3.12 manage.py collectstatic --noinpit --clear
+python 3.12 manage.py collectstatic --noinput --clear
 echo " BUILD END"
